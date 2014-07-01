a03fd127b5bb161d85761eaf03b4da293a725f85
Andrew Burton <abauckland@gmail.com>
2014-07-01 12:06:53 +0100

    image settings update 1

--- app/helpers/mjweb/contents_helper.rb
+++ app/helpers/mjweb/contents_helper.rb
@@ -4,12 +4,15 @@ module Mjweb
     def edit_links(content)
 
       case content.tile_id
-        when 1 ; return
-        when 2 ; return
+      when 1 ; return # transparent
+      when 2 ; return #colour tile
         when 3 ; link_to 'Edit Service Information', services_path, class: "btn btn-sm btn-primary btn"
-        when 4 ; link_to 'Edit Images', imagesettings_path(content.id), class: "btn btn-sm btn-primary btn"
-        when 5 ; link_to 'Edit About Page', abouts_path, class: "btn btn-sm btn-primary btn"
-        when 6 ; link_to 'Edit Opening Hours Information', edit_hour_path(current_user.id), class: "btn btn-sm btn-primary btn"
+        when 4 ; return #contact
+        when 5 ; link_to 'Edit Opening Hours Information', edit_hour_path(current_user.id), class: "btn btn-sm btn-primary btn"
+        when 6 ; link_to 'Edit Images', imagesettings_path(content.id), class: "btn btn-sm btn-primary btn"
+        when 7 ; return #e-commerce
+        when 8 ; return #location map     
+        when 9 ; link_to 'Edit About Page', abouts_path, class: "btn btn-sm btn-primary btn"
       end  
     end
     
