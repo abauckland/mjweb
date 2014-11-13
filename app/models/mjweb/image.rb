8f7d4ce1725bfa1f6030d0be4c5c87bf9e13e431
Andrew Burton <abauckland@gmail.com>
2014-11-13 11:18:55 +0000

    update image and info associations

--- app/models/mjweb/image.rb
+++ app/models/mjweb/image.rb
@@ -4,15 +4,8 @@ module Mjweb
     belongs_to :company
     mount_uploader :image, Mjweb::ImageUploader
     #relationships with models in same engine
-    has_many :imagesettings, :class_name => "Mjweb::Imagesetting"
-    has_many :abouts
-    has_many :services
-    has_many :books
-    has_many :banks
-    has_many :networkings
-    has_many :products    
-    has_many :trainings 
-    has_many :webpages  
+    has_many :imagesettings
+    has_many :infos 
 
   end
 end
--- app/models/mjweb/info.rb
+++ app/models/mjweb/info.rb
@@ -2,6 +2,7 @@ module Mjweb
   class Info < ActiveRecord::Base
     #relationship with model in main app
     belongs_to :page
+    belongs_to :image
 
     validates :title, :text, :image_id, presence: true
         
