dc35a8322b7bae61b5c93dcdba138e634afbcc64
Andrew Burton <abauckland@gmail.com>
2014-11-15 20:00:09 +0000

    add character count to info forms

--- app/assets/javascripts/mjweb/infos.js
+++ app/assets/javascripts/mjweb/infos.js
@@ -1,27 +1,47 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 jQuery(document).ready(function ($) {		
+
 	$('#info_image_id').ddslick({
 		attribute_id: "info_image_id",
 	    attribute_name: "info[image_id]",
-		width: 50,
+		width: 100,
 		//ddslick.js has been altered directly to get correct dropdown sise and content and provide parameters required by rails
 	});
 
-$('select#info_link_page_id').attr('disabled', 'disabled');
+	$('input#info_link_url').attr('disabled', 'disabled');
+	$('select#info_link_page_id').attr('disabled', 'disabled');
+		
+	$('input#url_no_url').click(function (){
+		$('input#page_id_page_url').prop('checked', false);
+		$('input#url_custom_url').prop('checked', false);
+		$('select#info_link_page_id').attr('disabled', 'disabled').val('');
+		$('input#info_link_url').attr('disabled', 'disabled').val('');
+	});
+	
+	$('input#url_custom_url').click(function (){
+		$('input#page_id_page_url').prop('checked', false);
+		$('input#url_no_url').prop('checked', false);
+		$('input#info_link_url').removeAttr('disabled');
+		$('select#info_link_page_id').attr('disabled', 'disabled').val('');
+	});
+	
+	$('input#page_id_page_url').click(function (){
+		$('input#url_custom_url').prop('checked', false);
+		$('input#url_no_url').prop('checked', false);
+		$('input#info_link_url').attr('disabled', 'disabled').val('');
+		$('select#info_link_page_id').removeAttr('disabled');
+	});
 
-$('input#url_custom_url').click(function (){
-	$('input#page_id_page_url').prop('checked', false);
-	$('input#info_link_url').removeAttr('disabled');
-	$('select#info_link_page_id').attr('disabled', 'disabled').val('');
-});
-
-$('input#page_id_page_url').click(function (){
-	$('input#url_custom_url').prop('checked', false);
-	$('input#info_link_url').attr('disabled', 'disabled').val('');
-	$('select#info_link_page_id').removeAttr('disabled');
-});
-
-
+	$('#info_text').keyup(function () {
+	  var max = 500;
+	  var len = $(this).val().length;
+	  if (len >= max) {
+	    $('.character_count').text(' you have reached the limit').css('color','red');
+	  } else {
+	    var char = max - len;
+	    $('.character_count').text(char + ' characters left').css('color','green');
+	  }
+	});
 
 });
