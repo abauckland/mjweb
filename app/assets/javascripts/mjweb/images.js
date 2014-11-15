f24b2d5a73e1b8cb9d12a2c26b259055ee7f820c
Andrew Burton <abauckland@gmail.com>
2014-11-15 20:01:32 +0000

    edit width of image select inputs

--- app/assets/javascripts/mjweb/images.js
+++ app/assets/javascripts/mjweb/images.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 jQuery(document).ready(function ($) {		
 
-	var select_width = 155;
+	var select_width = 100;
 	var select_height = 200;
 
 	$('#imagesetting_image_id').ddslick({
@@ -10,69 +10,5 @@ jQuery(document).ready(function ($) {
 	    attribute_name: "imagesetting[image_id]",
 		width: select_width,
 		selectText: "Select image...",
-	});
-	
-	$('#about_image_id').ddslick({
-		attribute_id: "about_image_id",
-	    attribute_name: "about[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});
-
-	$('#bank_image_id').ddslick({
-		attribute_id: "bank_image_id",
-	    attribute_name: "bank[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#book_image_id').ddslick({
-		attribute_id: "book_image_id",
-	    attribute_name: "book[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#networking_image_id').ddslick({
-		attribute_id: "networking_image_id",
-	    attribute_name: "networking[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#product_image_id').ddslick({
-		attribute_id: "product_image_id",
-	    attribute_name: "product[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#service_image_id').ddslick({
-		attribute_id: "service_image_id",
-	    attribute_name: "service[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#training_image_id').ddslick({
-		attribute_id: "training_image_id",
-	    attribute_name: "training[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});	
-
-	$('#webpage_image_id').ddslick({
-		attribute_id: "webpage_image_id",
-	    attribute_name: "webpage[image_id]",
-		width: select_width,
-		height: select_height,
-		selectText: "Select image...",
-	});			
+	});		
 });
