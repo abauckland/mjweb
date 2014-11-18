bae7469b333ec93d1a2f7c0df4ab9f74391ec72d
Andrew Burton <abauckland@gmail.com>
2014-11-18 11:23:15 +0000

    restrict deletion of images
    do not allow deletion of images when in use by info added

--- app/helpers/mjweb/images_helper.rb
+++ app/helpers/mjweb/images_helper.rb
@@ -2,8 +2,9 @@ module Mjweb
   module ImagesHelper
   
     def image_used(image)
-      image_used = Imagesetting.where(:image_id => image.id).first
-      if image_used.blank?
+      image_used_tile = Imagesetting.where(:image_id => image.id).first
+      image_used_info = Info.where(:image_id => image.id).first
+      if image_used_tile.blank? && image_used_info.blank? 
         return false
       else  
         return true
