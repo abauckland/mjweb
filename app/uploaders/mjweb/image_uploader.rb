765e0817f9a43307660bbe140af33a0eb14da24b
Andrew Burton <abauckland@gmail.com>
2014-12-22 20:28:28 +0000

    add thumbnails

--- app/uploaders/mjweb/image_uploader.rb
+++ app/uploaders/mjweb/image_uploader.rb
@@ -2,29 +2,28 @@
 module Mjweb
   class ImageUploader < CarrierWave::Uploader::Base
 
-#  include CarrierWave::RMagick
+  include CarrierWave::RMagick
 
   storage :fog
 
   def store_dir
     "uploads/#{model.company.name}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
-  
-#  process :resize_to_fill => [200, 200]
-#  process :auto_orient  
+    
+  process :resize_to_fill => [200, 200]
+  process :auto_orient  
 
-#  version :thumb do
-#    process :resize_to_fill => [50, 50]
-#    process :auto_orient
-#  end
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+    process :auto_orient
+  end
 
-#  def auto_orient
-#    manipulate! do |img|
-#      img = img.auto_orient
-#      img
-#    end
-#  end
+  def auto_orient
+    manipulate! do |img|
+      img = img.auto_orient
+      img
+    end
+  end
 
    def extension_white_list
      %w(jpg jpeg gif png)
