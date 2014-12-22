7dae23ce331b9e4665b62135cf672ebf294e4406
Andrew Burton <abauckland@gmail.com>
2014-12-22 22:26:20 +0000

    add width of select

--- app/assets/javascripts/mjweb/designs.js
+++ app/assets/javascripts/mjweb/designs.js
@@ -1,15 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 jQuery(document).ready(function ($) {		
+
 	$('#design_background_id').ddslick({
 		attribute_id: "design_background_id",
 	    attribute_name: "design[background_id]",
+	    width: 180,
 		//ddslick.js has been altered directly to get correct dropdown sise and content and provide parameters required by rails
 	});
 	
 	$('#design_font_id').ddslick({
 		attribute_id: "design_font_id",
 	    attribute_name: "design[font_id]",
+	    width: 180,
 		//ddslick.js has been altered directly to get correct dropdown sise and content and provide parameters required by rails
 	});
 
