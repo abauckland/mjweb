5fbdae64346d1f251f5d073c2c33c84b73025f2c
Andrew Burton <abauckland@gmail.com>
2015-03-25 15:28:02 +0000

    change logic

--- app/helpers/mjweb/pages_helper.rb
+++ app/helpers/mjweb/pages_helper.rb
@@ -4,11 +4,12 @@ module Mjweb
     def page_used(page)
      linktile = Mjweb::Linktile.joins(:page).where('mjweb_pages.id' => page.id).first
      infolink = Mjweb::Info.where(:link_page_id => page.id).first
-      if linktile.blank? && infolink.blank?
-        link_to '', page, :method => :delete, data: { confirm: 'Are you sure, all the information contained within the page will also be deleted?' }, class: 'line_delete_icon', title: "delete tile"
+      if linktile.blank? 
+        if infolink.blank?
+          link_to '', page, :method => :delete, data: { confirm: 'Are you sure, all the information contained within the page will also be deleted?' }, class: 'line_delete_icon', title: "delete tile"
+        end
       end
     end
-  
-  
+
   end
 end
