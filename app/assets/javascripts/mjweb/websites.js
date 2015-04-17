687ccf4c1a26807eb20c62620e32cfd69c2fc78e
andrew <admin@myhq.org.uk>
2015-04-17 14:35:25 +0100

    remove redundant js

--- app/assets/javascripts/mjweb/websites.js
+++ app/assets/javascripts/mjweb/websites.js
@@ -165,37 +165,6 @@ $(document).ready(function(){
 	});
 
 
-
-	$("div.help_tooltip").qtip({
-
-	    show: 'click',
-	    hide: 'unfocus',
-	    content: {
-	        text: function(event, api) {
-	                
-	            $.ajax({
-	                url: 'http://www.myhq.org.uk/helps/'+$(this).attr('id'),
-	                type: 'GET', // POST or GET
-	                dataType: 'html'
-	            })
-	            .then(function(content) {
-	                // Now we set the content manually (required!)
-	                api.set('content.text', content);
-	            }, function(xhr, status, error) {
-	                // Upon failure... set the tooltip content to the status and error value
-	                api.set('content.text', status + ': ' + error);
-	            });
-	            return 'Loading...';
-	       }
-	    },
-	    position: {
-	      my: 'top center',  // Position my top left...
-	      at: 'bottom center' // at the bottom right of...
-	    }
-	});
-
-
-
 	$(".multiple_images_tile").each(function(){
 		InfiniteRotator($(this));
 	});
